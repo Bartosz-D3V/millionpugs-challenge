2467290551ef855f844e23a0c5e66dcb603e3d45
Bartosz-D3V <nix.noxi379@gmail.com>
2022-01-30 11:23:12 +0100

    Add ArchUnit
    * Fix linting

--- src/test/kotlin/com/millionpugs/rules/EntityLocationRuleTest.kt
+++ src/test/kotlin/com/millionpugs/rules/EntityLocationRuleTest.kt
@@ -4,10 +4,6 @@ import com.tngtech.archunit.core.domain.JavaClasses
 import com.tngtech.archunit.junit.AnalyzeClasses
 import com.tngtech.archunit.junit.ArchTest
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Controller
-import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.RestController
 import javax.persistence.Entity
 import javax.persistence.Table
 
